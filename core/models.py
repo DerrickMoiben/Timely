f8e1b618a0c7c7a45b75d5fdcf77c538f7328246
DerrickMoiben <derrickmoio92@gmail.com>
2025-11-22 17:15:24 +0300

    defaulta can be null known that right now

--- core/models.py
+++ core/models.py
@@ -28,14 +28,30 @@ class CarwashSetup(models.Model):
 class BusinessWebsite(models.Model):
     business =  models.OneToOneField(BusinessInfo, on_delete=models.CASCADE, related_name='website')
     theme_color =  models.CharField(max_length=20, default='#28a745')
+    #hero section
+    hero_title = models.CharField(max_length=200, blank=True, default='') 
+    hero_subtitle = models.TextField(blank=True, default='' )
+    welcome_message = models.TextField(blank=True, default='')
+    
     about = models.TextField(blank=True)
     services = models.TextField(blank=True)
-    pricing_details = models.TextField(help_text="Provide pricing details for different services", default='10')
+    pricing_details = models.TextField(help_text="Provide pricing details for different services", default='')
     contact_email = models.EmailField(blank=True)
     contact_phone = models.CharField(max_length=20, blank=True)
-    address = models.CharField(max_length=255, blank=True, default='13')
-    location = models.CharField(max_length=255,  default='nairobi')
+    address = models.CharField(max_length=255, blank=True, default='')
+    location = models.CharField(max_length=255,  default='')
     closing_time = models.TimeField(default='10')
+   
+    #social links
+    facebook_url = models.URLField(blank=True, default='')
+    instagram_url = models.URLField(blank=True, default='')
+    twitter_url = models.URLField(blank=True, default='')
+    
+    # SEO
+    meta_title = models.CharField(max_length=200, blank=True, default='')
+    meta_description = models.TextField(blank=True, default='')
+    
+
     
     def __str__(self):
         return f"Website for {self.business.name}"
